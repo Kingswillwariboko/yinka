1439903030c7e012c10dd1ad32a28d5d10bd845a
admin <kingswillwariboko@gmail.com>
2022-09-30 06:14:51 -0700

    another changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,16 +13,7 @@ const Header = () => {
             <p> <Link to="/">YINKA AJIBOLA</Link></p>
         </div>
 
-        <div className='header__nav'>
-            <img src={menu} alt="" />
-
-            <ul>
-                <li> <Link to="/about">About me</Link></li>
-                <li>Ux projects</li>
-                <li>Resume</li>
-                <li>Contact me</li>
-            </ul>
-        </div>
+       
     </div>
   )
 }
